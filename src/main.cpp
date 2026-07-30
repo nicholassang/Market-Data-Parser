7fc7b2ee61fd2716a706c525be90f6153e1bdd04
nicholas <nicholasang.sg@gmail.com>
2026-07-30 23:49:51 +0800

    publisher subscriber model implemented (ring buffer)

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
+#include <thread>
+#include "parser.hpp"
 #include "udp_receiver.hpp"
 #include "market_packet.hpp"
 #include "ring_buffer.hpp"
 
 int main() {
     RingBuffer<MarketPacket, 1024> ringBuffer;
-    UDPReceiver receiver(9000, ringBuffer);
+    UDPReceiver receiver(9000,ringBuffer);
+    Parser parser(ringBuffer);
 
-    receiver.receive();
+    // @ threads to share the same resource ringBuffer
+    std::thread receiverThread([&]() {receiver.receive();});
+    std::thread parserThread([&]() {parser.process();});
+    receiverThread.join();
+    parserThread.join();
 
     return 0;
 }
